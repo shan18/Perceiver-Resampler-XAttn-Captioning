9f58ec939d51729d2b693e3463d1fdef9451360e
Shantanu Acharya <shantanu.acharya@nyu.edu>
2022-11-18 17:23:41 -0500

    add basic data and model loading functions to the main script

--- run.py
+++ run.py
@@ -2,23 +2,36 @@ import argparse
 import os
 
 from dataset import MLSLTDataset
+from model import MLSLTModel
 
 
-def create_dataset(data_root: str, data_type: str) -> MLSLTDataset:
+def create_dataset(data_root: str, data_type: str, batch_size: int, num_workers: int):
     """Creates the dataset and its dataloaders
 
     Args:
         data_root: The root directory of the dataset containing
             the video directory and the json file.
         data_type: The type of the dataset. Can be either `train`, `dev`, or `test`.
+        batch_size: The batch size of the dataloader.
+        num_workers: The number of workers for the dataloader.
 
     Returns:
-        The MLSLTDataset
+        The MLSLTDataset dataloader and the dataset object
     """
     dataset = MLSLTDataset(
         os.path.join(data_root, data_type), os.path.join(data_root, f'{data_type}.json')
     )
-    return dataset
+    loader = dataset.get_dataloader(batch_size, num_workers=num_workers)
+    return dataset, loader
+
+
+def main(args):
+    # Create dataloaders
+    _, train_loader = create_dataset(args.data_root, 'train', args.batch_size, args.num_workers)
+    _, dev_loader = create_dataset(args.data_root, 'dev', args.batch_size, args.num_workers)
+
+    # Create model
+    model = MLSLTModel()
 
 
 if __name__ == '__main__':
@@ -29,4 +42,8 @@ if __name__ == '__main__':
         default=os.path.join(BASE_DIR, 'data'),
         help='Path containing the train, dev and test dataset',
     )
+    parser.add_argument('--batch_size', type=int, default=32, help='Batch size')
+    parser.add_argument('--num_workers', type=int, default=8, help='Number of workers')
+    parser.add_argument('--learning_rate', type=float, default=1e-3, help='Learning rate')
+    parser.add_argument('--epochs', type=int, default=10, help='Number of epochs')
     args = parser.parse_args()
