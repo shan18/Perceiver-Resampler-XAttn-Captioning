808a2d7104c41ff3e8a4387b7cd649f05aeab014
Rakhee <rr3937@nyu.edu>
2022-11-21 00:33:06 -0500

    Add training engine

--- engine.py
+++ engine.py
@@ -1,10 +1,11 @@
+import os
 from tqdm import tqdm
 
 
 class Trainer:
 
     def __init__(self, model, optimizer, criterion, checkpoint_dir, device):
-        self.model = model
+        self.model = model.to(device)
         self.optimizer = optimizer
         self.criterion = criterion
         self.checkpoint_dir = checkpoint_dir
@@ -12,20 +13,40 @@ class Trainer:
 
     def train(self, loader):
         self.model.train()
-        pass
+        for video, transcript in tqdm(loader):
+            video, transcript = video.to(self.device), transcript.to(self.device)
+
+            self.optimizer.zero_grad()
+            outputs = self.model(video, transcript)
+
+            # Compute the loss
+            loss = self.criterion(outputs, transcript)
+
+            loss.backward()
+            self.optimizer.step()
 
     def fit(self, train_loader, dev_loader, epochs):
+        best_eval_loss = float('inf')
         for epoch in range(epochs):
             print(f'\nEpoch {epoch + 1}/{epochs}:')
             self.train(train_loader)
 
-            self.evaluate(dev_loader)
-
-            self.save_checkpoint()
+            eval_loss = self.evaluate(dev_loader)
+            if eval_loss < best_eval_loss:
+                best_eval_loss = eval_loss
+                self.save_checkpoint(epoch, eval_loss)
 
     def evaluate(self, loader):
         self.model.eval()
-        pass
+        eval_loss = 0
+        with torch.no_grad():
+            for video, transcript in tqdm(loader):
+                video, transcript = video.to(self.device), transcript.to(self.device)
+                outputs = self.model(video, transcript)
+                loss = self.criterion(outputs, transcript)
+                eval_loss += loss.item()
 
-    def save_checkpoint(self):
-        pass
+        return eval_loss / len(loader)
+
+    def save_checkpoint(self, epoch, loss):
+        torch.save(self.model.state_dict(), os.path.join(self.checkpoint_dir, f'ckpt_ep{epoch}_loss{loss}.pt'))
