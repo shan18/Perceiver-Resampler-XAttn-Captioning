fa24070c5d125792ca1764a07c6b700bbf18c3e8
Shantanu Acharya <shantanu.acharya@nyu.edu>
2022-12-02 20:56:38 -0500

    add usage examples on test mode

--- run.py
+++ run.py
@@ -5,15 +5,32 @@ Multilingual Sign Language Translation dataset.
 Usage:
 ======
 
+Training
+--------
+
 HYDRA_FULL_ERROR=1 python run.py \
     --config-path=<Directory containing the config file> \
     --config-name=config.yaml \
     name=<name of the experiment> \
+    mode=train \
     dataset.train_ds.video_dir=<Directory containing the videos for train set> \
     dataset.train_ds.json_path=<Path to the json file with the transcripts for train set> \
     dataset.validation_ds.video_dir=<Directory containing the videos for validation set> \
     dataset.validation_ds.json_path=<Path to the json file with the transcripts for validation set> \
     trainer.exp_dir=<Directory to save the checkpoints and logs>
+
+Testing
+-------
+
+HYDRA_FULL_ERROR=1 python run.py \
+    --config-path=<Directory containing the config file> \
+    --config-name=config.yaml \
+    name=<name of the experiment> \
+    mode=test \
+    pretrained_name=<path to the checkpoint> \
+    dataset.test_ds.video_dir=<Directory containing the videos for test set> \
+    dataset.test_ds.json_path=<Path to the json file with the transcripts for test set> \
+    trainer.exp_dir=<Directory to save the logs>
 """
 
 
