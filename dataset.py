0bf530ab13d2563020a2a49650403d4cda3d1528
Shantanu Acharya <shantanu.acharya@nyu.edu>
2022-12-11 04:41:44 -0500

    add support for multiple sign language video loading in the dataset

--- dataset.py
+++ dataset.py
@@ -4,41 +4,64 @@ This script contains the Dataset class and the fuctions to create its dataloader
 
 import json
 import os
+import random
 
 import torch
 from torch.utils.data import DataLoader, Dataset
 from torchvision.io import read_video
 from transformers import CLIPProcessor, GPT2Tokenizer
 
+SUPPORTED_LANGUAGES = ['zh', 'uk', 'ru', 'bg', 'is', 'de', 'it', 'sv', 'lt', 'en']
+
 
 class MLSLTDataset(Dataset):
-    def __init__(self, video_dir, json_path):
+    def __init__(self, video_dir, json_path, sign_languages=['en']):
         super().__init__()
 
         assert os.path.exists(video_dir), 'The videos directory does not exist.'
+        for sign_lang in sign_languages:
+            assert sign_lang in SUPPORTED_LANGUAGES, f'{sign_lang} sign language not supported.'
 
+        self.sign_languages = sign_languages
         self.video_dir = video_dir
-        self._get_labels(json_path)
+        self._get_samples(json_path)
 
         self.image_processor = CLIPProcessor.from_pretrained('openai/clip-vit-base-patch32')
 
         self.tokenizer = GPT2Tokenizer.from_pretrained('gpt2')
         self.tokenizer.pad_token = '<|pad|>'
 
-    def _get_labels(self, json_path):
-        """Reads the json file and creates a label dictionary"""
+    def _get_samples(self, json_path):
+        """Reads the json file and create dataset samples."""
         with open(json_path) as f:
             metadata = json.load(f)
 
-        self.json_data = [
-            {'id': video['id'], 'transcript': {label['lang']: label['text'] for label in video['sign_list']}}
-            for video in metadata
-        ]
+        self.videos = []
+        self.transcripts = {}
+        for video_data in metadata:
+            # Get video samples
+            self.videos.extend(
+                [
+                    {
+                        'id': video_data['id'],
+                        'lang': sign_lang,
+                    }
+                    for sign_lang in self.sign_languages
+                ]
+            )
 
-        self.max_length = max([len(x['transcript']['en']) for x in self.json_data])
+            # Get transcripts
+            self.transcripts[video_data['id']] = {
+                text_data['lang']: text_data['text'] for text_data in video_data['sign_list']
+            }
+
+        random.shuffle(self.videos)
+
+        # Get max length of transcripts
+        self.max_length = max([len(x['en']) for x in self.transcripts.values()])
 
     def __len__(self):
-        return len(self.json_data)
+        return len(self.videos)
 
     def _process_video(self, video_path):
         video, _, _ = read_video(video_path, output_format='TCHW', pts_unit='sec')
@@ -65,9 +88,9 @@ class MLSLTDataset(Dataset):
         return tokens, attention_mask
 
     def __getitem__(self, index):
-        sample_id = self.json_data[index]['id']
-        transcript = self.json_data[index]['transcript']['en']
-        video_path = os.path.join(self.video_dir, str(sample_id), 'en.mp4')
+        sample_id = self.videos[index]['id']
+        video_path = os.path.join(self.video_dir, str(sample_id), f'{self.videos[index]["lang"]}.mp4')
+        transcript = self.transcripts[sample_id]['en']
 
         # Process video
         video, video_length = self._process_video(video_path)
